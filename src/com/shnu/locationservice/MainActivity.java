16fb8b9fe6544bc8b39aad9c6db8ca6c14f23ffc
hsm <hsm6918003@gmail.com>
2015-12-31 20:48:51 +0800

    添加用户注册信息界面

--- src/com/shnu/locationservice/MainActivity.java
+++ src/com/shnu/locationservice/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends Activity {
 
 	} ;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
--- src/com/shnu/locationservice/mms/Subject.java
+++ src/com/shnu/locationservice/mms/Subject.java
@@ -1,7 +1,15 @@
 package com.shnu.locationservice.mms;
 
+
+import com.shnu.locationservice.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Subject extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.subject);
+	}
 
 }
